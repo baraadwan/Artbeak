30e7a9f00bb4b93f985fc721171b62591a0ea3b1
Baradwan58 <128212215+Baradwan58@users.noreply.github.com>
2025-09-06 21:45:11 +0300

    clariy added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Clarity Analytics Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "t6nesxgcgt");`,
+          }}
+        />
+      </head>
       <body
         className={`${interFont.variable} ${playfairDisplay.variable} antialiased font-inter`}
       >
