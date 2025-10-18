645d9590feb58215cf422f1069068c1e9f9d910d
baraadwan <bara@artbeak.com>
2025-10-19 02:06:01 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,8 +37,8 @@ export default function RootLayout({
             })(window, document, "clarity", "script", "t6nesxgcgt");`,
           }}
         />
-        <script>
-  !function(t,e){var o,n,p,r;e._SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e._SV=1)}(document,window.posthog||[]);
+<script>
+  !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
   
   console.log('Initializing PostHog with host: https://us.i.posthog.com');
   
@@ -58,21 +58,22 @@ export default function RootLayout({
   });
   
   // Identify user with UXbreak tracking code
-  posthog.identify('uxb_582wbqxgh');
+  posthog.identify('uxb_v6g5541rw');
   
   // Set user properties
   posthog.people.set({
-    'uxbreak_tracking_code': 'uxb_582wbqxgh',
+    'uxbreak_tracking_code': 'uxb_v6g5541rw',
     'account_type': 'uxbreak_user'
   });
   
   // Send initialization event
   posthog.capture('uxbreak_tracking_initialized', {
-    tracking_code: 'uxb_582wbqxgh',
+    tracking_code: 'uxb_v6g5541rw',
     timestamp: new Date().toISOString()
   });
   
   console.log('UXbreak Analytics fully initialized with PostHog');
+</script>
 </script>
       </head>
       <body
